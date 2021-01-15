fcaee64135ce69ad89bdfd485c181fe68f496fc6
Gaurav Das <gauravdas014@gmail.com>
2021-01-15 18:30:13 +0530

    change the backend folder name

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require('http');
+const app = require('./app');
+const db = require('./database')();
+
+const port = process.env.PORT || 8080;
+
+// this is useful when creating a https server
+http
+  .createServer(app)
+  .listen(port, () => console.log('HTTP server listening on ' + port));
